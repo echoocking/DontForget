6013297136f54c7555b1b957b2d74062592cb753
echoocking <echoocking@gmail.com>
2020-01-07 23:38:41 +0800

    [A] [LC]Iäº叉树 节点定义

---
+++ leetcode/938_bst_range_sum.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.left_node = None
+        self.right_node = None
+
+
+class BST:
+
+    def insert(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def search(self, value):
+        pass
+
+
