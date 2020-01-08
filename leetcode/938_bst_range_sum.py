6df6368d5b73db17988907be25702d7244a8d62e
echoocking <echoocking@gmail.com>
2020-01-09 00:46:26 +0800

    [A][LC][938] bst插入，查询。（未校验）

--- leetcode/938_bst_range_sum.py
+++ leetcode/938_bst_range_sum.py
@@ -11,14 +11,35 @@ class Node:
 
 class BST:
 
-    def insert(self):
+    def __init__(self, root_value=None):
+        self.root = root_value
+
+    def insert(self, value):
         """
         二叉树的规则是 小于当前节点的值放在左子树，大于当前节点的值放在右子树。所以插入的时候先要查找。
         如果这个值小于节点值，就转左，大于节点值就转右。如果这时 节点对应方向的子节点为空(叶子节点)，就创建新节点，把值插入进去。结束。
         如果对应的子节点不为空，那就再继续进行查找，直到有对应的子节点为空为止。
         :return:
         """
-        pass
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert(value, self.root)
+
+    def _insert(self, value, node):
+        if value > node.value:
+            if node.right_node is None:
+                node.right_node = Node(value)
+                return True
+            else:
+                self._insert(value, node.right_node)
+
+        elif value < node.value:
+            if node.left_node is None:
+                node.left_node = Node(value)
+                return True
+            else:
+                self._insert(value, node.left_node)
 
     def delete(self):
         """
@@ -29,12 +50,29 @@ class BST:
         pass
 
     def search(self, value):
+        return self._search(value, self.root)
+
+    def _search(self, value, node):
         """
         小于转左，大于转右，直到相等或者下一个对应方向为空。
         好像还有几种遍历方式哦
+        递归写 挺直观的
         :param value:
         :return:
         """
-        pass
 
+        if node is None:
+            return False
+
+        else:
+            if value == node.value:
+                return True
+
+            if value > node.value:
+                self._search(value, node.right_node)
+            else:
+                self._search(value, node.left_node)
+
+    def walk(self):
+        pass
 
